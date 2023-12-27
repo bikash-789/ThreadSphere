91612a58d7931b2afb1e76579b675b5d223fe5f2
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-12-27 22:52:33 +0530

    Added timezone for date and time formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,6 +18,7 @@ export function formatDateString(dateString: string) {
     year: "numeric",
     month: "short",
     day: "numeric",
+    timeZone: "Asia/Kolkata",
   };
 
   const date = new Date(dateString);
