77008f71edf1999d69f7d954d6dc87ae05c75dc2
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-12-30 00:47:04 +0530

    Bug fix: New user onboarding added on home route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 export default async function Home() {
   const results = await fetchPosts(1, 30);
   const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <>
