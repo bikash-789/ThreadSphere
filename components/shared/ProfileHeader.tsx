708e0a1bda3e4318a54750495b7b7ab21016b45e
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-12-29 15:35:45 +0530

    Added radix-ui/themes to the card components for better UI design

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
 interface Props {
@@ -22,14 +23,7 @@ const ProfileHeader = ({
     <div className="flex w-full flex-col justify-start">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <div className="relative h-20 w-20 object-cover">
-            <Image
-              src={imgUrl}
-              alt="profile image"
-              fill
-              className="rounded-full object-cover shadow-2xl"
-            />
-          </div>
+          <Avatar src={imgUrl} fallback={name[0]} alt={username} size={"6"} />
           <div className="flex-1">
             <h2 className="text-left text-heading3-bold text-light-1">
               {name}
