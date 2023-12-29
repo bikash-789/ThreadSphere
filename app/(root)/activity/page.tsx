adf2a4921fa935a3c9e69c6271bbff73c31821cb
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-12-29 21:45:16 +0530

    UI update -  Reduce the padding in activity card

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -17,7 +17,7 @@ const Page = async () => {
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
-      <section className="mt-10 flex flex-col gap-5">
+      <section className="flex flex-col gap-3">
         {activities &&
           activities.length > 0 &&
           activities.map((activity: any) => {
@@ -34,7 +34,7 @@ const Page = async () => {
                     : activity.threadId
                 }`}
               >
-                <article className="activity-card">
+                <article className="activity-card !py-2">
                   <Avatar
                     src={
                       activity.type == "reply"
